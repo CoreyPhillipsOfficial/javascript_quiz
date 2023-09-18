f73c2c1247a46338decf27dbcba2f4b7550f7384
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-09-18 01:27:48 -0400

    worked on highscore functions

--- js/viewhighscores.js
+++ js/viewhighscores.js
@@ -4,7 +4,7 @@ var scoreFormWrap = document.querySelector('.score-form-wrap');
 var scoreOutput = document.querySelector('#score-output');
 var scoreForm = document.querySelector('#score-form');
 var score = localStorage.getItem('score');
-
+var clearScoresBtn = document.querySelector('#clear-scores');
 
 function getHighscores() {
     return JSON.parse(localStorage.getItem('highscores')) || [];
@@ -32,12 +32,14 @@ function showScoreOutput() {
 
     var highscores = getHighscores();
 
+    scoresOutputDiv.innerHTML = '';
+
     if (!highscores.length) {
         scoresOutputDiv.innerHTML = '<p>No scores have been saved</p>';
     }
 
     highscores.forEach(function (scoreObj) {
-        scoresOutputDiv.insertAdjacentElement('beforeend', `
+        scoresOutputDiv.insertAdjacentHTML('beforeend', `
         <div>
             <h3>Name: ${scoreObj.name}</h3>
             <p>Score: ${scoreObj.score}</p>
@@ -47,12 +49,22 @@ function showScoreOutput() {
 
 }
 
-
 function showScore() {
     var score = localStorage.getItem('score');
     scoreOutput.innerText = 'Your high score: ' + score;
 }
 
+function clearHighScores() {
+    localStorage.removeItem('highscores');
+}
+
+
+
 showScoresBtn.addEventListener('click', showScoreOutput);
 scoreForm.addEventListener('submit', saveHighscore);
 showScore();
+
+clearScoresBtn.addEventListener('click', function () {
+    clearHighScores();
+    scoresOutputDiv.innerHTML = '<p>No scores have been saved</p>'
+})
